391281e454812a12dc5e4f1f6f5d8fd6ec4b22a1
Zhi.Huang <zh.impact@outlook.com>
2025-05-18 13:37:35 +0800

    feat: integrate random cat image API and update site branding

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://cdn2.thecatapi.com/images/**")],
+  },
 };
 
 export default nextConfig;
